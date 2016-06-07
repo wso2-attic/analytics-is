8cc6894721cc248f2104f31956575dd21208f28d
cdathuraliya <cdathuraliya@gmail.com>
2016-06-07 17:32:56 +0530

    Add line chart gadget

---
+++ features/org.wso2.carbon.analytics.is.feature/src/main/capp/Line_Chart/js/gadgetconf.js
@@ -0,0 +1,31 @@
+var charts = [{
+    name: ROLE_SESSION_COUNT_OVER_TIME,
+    columns: ["sessionCount","timestamp", "sessionType"],
+    schema: [{
+        "metadata": {
+            "names": ["sessionCount","timestamp", "sessionType"],
+            "types": ["linear","time", "ordinal"]
+        },
+        "data": []
+    }],
+    chartConfig: {
+        x: "timestamp",
+        charts: [{ type: "line", range:"true",  y : "sessionCount", color: "sessionType" }],
+        padding: { "top": 30, "left": 60, "bottom": 60, "right": 100 },
+        range: true,
+    },
+    types: [
+        { name: TYPE_SESSIONS, type: 23 }
+    ],
+    processData: function(data) {
+        var result = [];
+        var schema = this.schema;
+        var columns = this.columns;
+        var data = data[0];
+        data.forEach(function(row) {
+            var record = [row.sessionCount, row.timestamp, row.sessionType];
+            result.push(record);
+        });
+        return result;
+    }
+}];
