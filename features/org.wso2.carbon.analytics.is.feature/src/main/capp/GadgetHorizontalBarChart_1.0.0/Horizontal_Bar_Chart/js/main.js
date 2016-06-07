fbbf0adfa49e6344a2580f481f1ac5038edce3ba
manoramahp <manorama@wso2.com>
2016-06-07 11:46:26 +0530

    Fixed https://wso2.org/jira/browse/ANLYIS-5

--- features/org.wso2.carbon.analytics.is.feature/src/main/capp/GadgetHorizontalBarChart_1.0.0/Horizontal_Bar_Chart/js/main.js
+++ features/org.wso2.carbon.analytics.is.feature/src/main/capp/GadgetHorizontalBarChart_1.0.0/Horizontal_Bar_Chart/js/main.js
@@ -683,23 +683,23 @@ var substringMatcher = function() {
 
         switch (filterType) {
             case 12: {
-                listnedAdditionalUserPrefs= " AND userName:"+ q +"*";
+                listnedAdditionalUserPrefs= " AND userName:\""+ q +"*\"";
                 break;
             }
             case 13: {
-                listnedAdditionalUserPrefs= " AND serviceprovider:"+ q +"*";
+                listnedAdditionalUserPrefs= " AND serviceprovider:\""+ q +"*\"";
                 break;
             }
             case 14: {
-                listnedAdditionalUserPrefs= " AND identityProvider:"+ q +"*";
+                listnedAdditionalUserPrefs= " AND identityProvider:\""+ q +"*\"";
                 break;
             }
             case 15: {
-                listnedAdditionalUserPrefs= " AND role:"+ q +"*";
+                listnedAdditionalUserPrefs= " AND role:\""+ q +"*\"";
                 break;
             }
             case 16: {
-                listnedAdditionalUserPrefs= " AND userStoreDomain:"+ q +"*";
+                listnedAdditionalUserPrefs= " AND userStoreDomain\":"+ q +"*\"";
                 break;
             }
             default : {
