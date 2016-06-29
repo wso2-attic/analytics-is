dbed316688696cdd5ab81b371496146dc7bdd44d
cdathuraliya <cdathuraliya@gmail.com>
2016-06-29 16:09:01 +0530

    Add session data table

--- features/org.wso2.carbon.analytics.is.feature/src/main/capp/IsAnalytics-Gadget-MessageTable_1.0.0/Message_Table/js/gadgetconf.js
+++ features/org.wso2.carbon.analytics.is.feature/src/main/capp/IsAnalytics-Gadget-MessageTable_1.0.0/Message_Table/js/gadgetconf.js
@@ -23,5 +23,22 @@ var configs = [{
             { name: "authenticationSuccess", label: "Authentication Success", type: "ordinal" },
             { name: "_timestamp", label: "Timestamp", type: "ordinal" }
         ]
-    }
+    },
+    {
+        name: TYPE_SESSIONS,
+        type: 29,
+        columns: [
+            { name: "sessionId", label: "Session ID", type: "ordinal" },
+            { name: "startTimestamp", label: "Start Time", type: "ordinal" },
+            { name: "renewTimestamp", label: "Renew Time", type: "ordinal" },
+            { name: "terminationTimestamp", label: "Termination Time", type: "ordinal" },
+            { name: "action", label: "Action", type: "ordinal" },
+            { name: "userName", label: "Username", type: "ordinal" },
+            { name: "userstoreDomain", label: "Userstore Domain", type: "ordinal" },
+            { name: "IP", label: "IP", type: "ordinal" },
+            { name: "tenantDomain", label: "Tenant Domain", type: "ordinal" },
+            { name: "rememberMeFlag", label: "Remember Me Flag", type: "ordinal" },
+            { name: "_timestamp", label: "Timestamp", type: "ordinal" }
+        ]
+    }    
 ];
