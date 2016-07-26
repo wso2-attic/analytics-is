28ed23b7d61b4b1ce49dd4f278c3cf4ddf7d62e1
mohan <mohan.vive@gmail.com>
2016-07-26 11:01:33 +0530

    increasing the datapublisher time.

--- product/integration/tests-integration/tests/src/test/java/org/wso2/das4is/integration/tests/is/ISAuthenticationAnalyticsTestCase.java
+++ product/integration/tests-integration/tests/src/test/java/org/wso2/das4is/integration/tests/is/ISAuthenticationAnalyticsTestCase.java
@@ -123,7 +123,7 @@ public class ISAuthenticationAnalyticsTestCase extends DASIntegrationTest {
                 sampleEventList.add(event);
             }
             dataPublisherClient.publish(sampleEventList);
-            Thread.sleep(25000);
+            Thread.sleep(35000);
             dataPublisherClient.shutdown();
             Thread.sleep(10000);
 
